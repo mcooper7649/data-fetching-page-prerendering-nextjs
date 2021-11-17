07457cacf5c91186a33a9e05865991000a9b7e8e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-11-17 00:05:58 -0500

    added getServerSideProps function and destructered context

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -5,6 +5,9 @@ function userProfilePage(props) {
 export default userProfilePage;
 
 export async function getServerSideProps(context){
+    const { params, req, res } = context;
+    console.log(req)
+    console.log(res)
     return {
         props: {
             username: 'Max'
